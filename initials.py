cbd1fc0d2798d4fada90caa6ba59919d7b31bc70
Stephen Anim <2425402035@live.gctu.edu.gh>
2026-06-01 09:40:16 +1000

    final work

--- initials.py
+++ initials.py
@@ -1,6 +1,33 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment NUmber: 2
+#Name: Stephen Anim
+#Student ID: 2425402035
+#Email: stephenanim45@gmail.com
+#Grader:
+#On my honor, Stephen Anim, this programming assignment is my own work
+#and i have not provided this code to another student.
 
 def main():
-  pass #code goes here
+    #print the stylized initials in a 60 character wide grid
+    print()
+    print('...SAS')
+    print()
+
+
+print('...SSSSSSSSSSSS...**...AAAA....AAAA......SSSSSSSSSSSS...**')
+print('...SSSSSSSSSSSS...**...AAAA....AAAA......SSSSSSSSSSSS...**')
+print('...SSSS...........**...AAAA....AAAA......SSSS............**')
+print('...SSSS...........**...AAAA....AAAA......SSSS............**')
+print('...SSSSSSSSSSSS...**...AAAAAAAAAAAA......SSSSSSSSSSSS...**')
+print('...SSSSSSSSSSSS...**...AAAAAAAAAAAA......SSSSSSSSSSSS...**')
+print('...........SSSS...**AAAA....AAAA...........SSSS...**')
+print('...........SSSS...**AAAA....AAAA...........SSSS...**')
+print('...SSSSSSSSSSSS...**...AAAA....AAAA......SSSSSSSSSSSS**')
+print()
+
+#
+#
 
 
 
